be8d1fefb47a610f3d11c3871bbac417b62cc464
Eduardo Quirino SanRomán <33359843+EduardoQSR@users.noreply.github.com>
2020-08-11 22:31:40 -0500

    Primer commit

---
+++ src/Operaciones/Operaciones.java
@@ -0,0 +1,30 @@
+package Operaciones;
+public class Operaciones
+{
+    private double suma;
+    private double resta;
+    private double multiplicacion;
+    private double division;
+    
+    public double suma(double x, double y)
+    {
+        return x+y;
+    }
+    
+    public double resta(double x, double y)
+    {
+        return x-y;
+    }
+    
+    public double multiplicacion(double x, double y)
+    {
+        return x*y;
+    }
+    
+    public double division(double x, double y)
+    {
+        return x/y;
+    }
+}
+
+
